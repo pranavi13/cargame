8f62e90eacf7f984eacb5cb8549f4fa25713eb08
pranavi13 <78367057+pranavi13@users.noreply.github.com>
2021-06-09 21:23:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var canvas ;
+var gameState=0;
+var playerCount;
+var database;
+var form,game, player;
+
+function setup(){
+    canvas= createCanvas(400,400);
+    database=firebase.database();  
+      game=new Game();
+    game.getState();
+    game.start();
+}
+function draw(){}
